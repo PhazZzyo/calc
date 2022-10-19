6823e11eaa13fdeede93d2404eab5ea5360e9c45
Alex <alex.phazz@gmail.com>
2022-10-19 15:18:36 +0300

    Gem created

---
+++ lib/calc.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative "calc/version"
+
+module Calc
+  class Error < StandardError; end
+  
+puts "Please enter your example with space between each element: "
+
+example = gets.to_s.split
+
+a = example[0].to_f
+b = example[1].to_s
+c = example[2].to_f
+
+  case b
+    when "+"        
+        puts "The result is: #{a + c}"        
+
+    when "-"        
+        puts "The result is: #{a - c}"        
+
+    when "*"        
+        puts "The result is: #{a * c}"        
+
+    when "/"        
+        puts "The result is: #{a / c}"        
+    else 
+        puts "Sorry, you entered an invalid operator, only +, -, *, / are allowed!"
+  end
+end
---
+++ lib/calc/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Calc
+  VERSION = "0.1.0"
+end
